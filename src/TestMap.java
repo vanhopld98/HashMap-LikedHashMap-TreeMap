11e0d369f56576c4bd112086b9bb7569e0f518ea
vanhopld98 <vanhopld98@gmail.com>
2021-08-19 11:17:48 +0700

    done

---
+++ src/TestMap.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TestMap {
+    public static void main(String[] args) {
+        Map<String,Integer> hashMap = new HashMap<>();
+        hashMap.put("Smith",30);
+        hashMap.put("Anderson",31);
+        hashMap.put("Lewis",29);
+        hashMap.put("Cook",29);
+        System.out.println("Display entries in HashMap");
+        System.out.println(hashMap+"\n");
+
+        Map<String,Integer> treeMap = new TreeMap<>();
+        treeMap.put("Smith",30);
+        treeMap.put("Anderson",31);
+        treeMap.put("Lewis",29);
+        treeMap.put("Cook",29);
+        System.out.println("Display entries in TreeMap");
+        System.out.println(treeMap+"\n");
+
+        Map<String,Integer> linkedHashMap = new LinkedHashMap<>(16,0.75f,true);
+        linkedHashMap.put("Smith",30);
+        linkedHashMap.put("Anderson",31);
+        linkedHashMap.put("Lewis",29);
+        linkedHashMap.put("Cook",29);
+        System.out.println("Display entries in LinkedHashMap");
+        System.out.println("The age for "+"Lewis is "+linkedHashMap.get("Lewis"));
+    }
+}
